81297b1c7c38aefc588f13c21c9b488bb4559c97
Biruk Haileye Tabor <biruk.haileye@gmail.com>
2025-03-29 11:02:46 +0300

    Add turbo 8.0.13

---
+++ installer/templates/phx_web/channels/turbo/turbo_stream_channel.ex
@@ -0,0 +1,17 @@
+defmodule <%= @web_namespace %>.Turbo.TurboStreamChannel do
+  use <%= @web_namespace %>, :channel
+
+  @impl true
+  def join(topic, %{"signed_topic" => signed_topic}, socket) do
+    case <%= @app_module %>.Token.verify(socket, signed_topic) do
+      {:ok, ^topic} ->
+        {:ok, socket}
+      _ ->
+        {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  def join(_topic, _payload, _socket) do
+    {:error, %{reason: "unauthorized"}}
+  end
+end
